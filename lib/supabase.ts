9b6891a770df4022ffe2da9f5651d87e56d4cf2c
Opa Kholis Majid <opakholis.id@gmail.com>
2021-12-07 15:22:23 +0700

    chore: tailwindcss and supabase setup

---
+++ lib/supabase.ts
@@ -0,0 +1,6 @@
+import { createClient } from '@supabase/supabase-js';
+
+const apiUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || '';
+const apiKey = process.env.NEXT_PUBLIC_SUPABASE_KEY || '';
+
+export const supabase = createClient(apiUrl, apiKey);
