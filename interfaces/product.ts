0e322ebb2298ec591118a426ae48961fcbf08ada
Opa Kholis Majid <opakholis.id@gmail.com>
2021-12-07 16:38:26 +0700

    feat: query data from supabase

---
+++ interfaces/product.ts
@@ -0,0 +1,6 @@
+export interface Product {
+  id: number;
+  created_at: string;
+  name: string;
+  description: string;
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,14 @@
 import Head from 'next/head';
+import { supabase } from 'lib/supabase';
+import { GetStaticProps } from 'next';
 
-export default function Home() {
+import { Product } from 'interfaces/product';
+
+interface Props {
+  products: Product[];
+}
+
+export default function Home({ products }: Props) {
   return (
     <div className="w-full max-w-screen-md p-4 mx-auto">
       <Head>
@@ -12,7 +20,25 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className="flex space-x-3">Hi</main>
+      <main className="flex space-x-4 mt-12">
+        {products.map((product) => (
+          <div
+            key={product.id}
+            className="bg-gray-100 border border-transparent rounded-md transition duration-200 space-y-1 p-4 hover:border-blue-400"
+          >
+            <h1 className="font-semibold text-xl">{product.name}</h1>
+            <p className="text-gray-700">{product.description}</p>
+          </div>
+        ))}
+      </main>
     </div>
   );
 }
+
+export const getStaticProps: GetStaticProps = async () => {
+  const { data: products } = await supabase.from(`products`).select(`*`);
+
+  return {
+    props: { products },
+  };
+};
